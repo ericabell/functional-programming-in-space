add6b6e8a7119117ee240381ba89fd5af528cc2f
Eric Abell <ericabell@users.noreply.github.com>
2017-09-24 10:44:43 -0500

    Template classes in place, beginning required edits

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Spaceship> ships = new ArrayList<>();
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            ships.add(new Spaceship(r));
+        }
+
+        for (Spaceship ship : ships) {
+            System.out.println(ship);
+        }
+
+        System.out.println("Ship with most guns of size >=5");
+        Spaceship best = Spaceship.findMostGunsWithSize(ships, 5);
+        System.out.println(best);
+
+        System.out.println("Ships with most guns of size >=12");
+        Spaceship best12 = Spaceship.findMostGunsWithSize(ships, 12);
+        System.out.println(best12);
+
+        System.out.println("Fighting monsters");
+        Spaceship.sendShipsToFightMonsters(ships);
+
+        System.out.println("Sum of firepower defending the Earth is:");
+        System.out.println(Spaceship.sumOfEarthGuns(ships));
     }
 }
---
+++ src/Spaceship.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Spaceship {
+    int numLasers;
+    int size;
+    boolean hasWarpDrive;
+    String name;
+
+    static int nameCounter = 0;
+    public static final String[] names = {"Enterprise", "Voyager", "Wuddship", "Ariel", "Pegasus",
+            "Defiant", "Valiant", "Victory", "Trafalgar", "Shadow"};
+
+    public Spaceship (int numLasers, int size, boolean hasWarpDrive, String name) {
+        this.numLasers = numLasers;
+        this.size = size;
+        this.hasWarpDrive = hasWarpDrive;
+        this.name = name;
+    }
+
+    public Spaceship (Random r) {
+        if (r == null) {
+            r = new Random();
+        }
+        this.numLasers = r.nextInt(20);
+        this.size = r.nextInt(15);
+        this.hasWarpDrive = r.nextBoolean();
+
+        this.name = names[nameCounter];
+        nameCounter++;
+    }
+
+    public void fightSpaceMonster () {
+        Random random = new Random();
+        int spaceMonsterHealth = random.nextInt(12);
+        if (numLasers >= spaceMonsterHealth) {
+            System.out.println(name + " wins the fight.");
+        } else {
+            System.out.println(name + " lost the fight.");
+        }
+    }
+
+    public String toString () {
+        return "Name: " + name + "\nLasers: " + numLasers + "\nSize: " + size + "\nWarpDrive: " + hasWarpDrive;
+    }
+
+    //Write these functions using functional programming:
+    public static int sumOfEarthGuns (List<Spaceship> ships) {
+        return -1;
+    }
+
+    public static void sendShipsToFightMonsters (List<Spaceship> ships) {
+
+    }
+
+    public static Spaceship findMostGunsWithSize(List<Spaceship> ships, int size) {
+        return null;
+    }
+
+
+}
